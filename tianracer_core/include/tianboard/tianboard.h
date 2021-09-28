25b95e3a5f194e2c77e6b1e955773677b54dd8e3
chargerKong <chargerKong@126.com>
2021-09-28 11:35:36 +0800

    convert core constructor to ros2

--- tianracer_core/include/tianboard/tianboard.h
+++ tianracer_core/include/tianboard/tianboard.h
@@ -1,45 +1,47 @@
 #ifndef __TIANBOARD_H__
 #define __TIANBOARD_H__
 
-#include "ros/ros.h"
+#include <rclcpp/rclcpp.hpp>
 #include "serial.h"
-#include "geometry_msgs/Twist.h"
-#include "geometry_msgs/Pose2D.h"
-#include "geometry_msgs/TransformStamped.h"
-#include "nav_msgs/Odometry.h"
+#include "geometry_msgs/msg/twist.hpp"
+#include "geometry_msgs/msg/pose2_d.hpp"
+#include <geometry_msgs/msg/transform_stamped.hpp>
 #include "boost/bind.hpp"
 #include "boost/function.hpp"
-#include <tf/transform_broadcaster.h>
-#include "sensor_msgs/Imu.h"
-#include "ackermann_msgs/AckermannDrive.h"
+
+#include <nav_msgs/msg/odometry.hpp>
+#include <tf2_ros/transform_broadcaster.h>
+#include "sensor_msgs/msg/imu.hpp"
+#include <ackermann_msgs/msg/ackermann_drive.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 #define DEFAULT_SERIAL_DEVICE "/dev/ttyUSB0"
 
 using namespace std;
 using namespace boost;
 
-class Tianboard {
+class Tianboard: public rclcpp::Node {
 public:
-    Tianboard(ros::NodeHandle *nh);
-    //~Tianboard();
+    Tianboard();
+    ~Tianboard();
 private:
-    ros::Publisher odom_pub_;
-    ros::Publisher uwb_pub_;
-    ros::Publisher imu_pub_;
-    ros::Subscriber cmd_vel_sub_;
-    ros::Subscriber ackermann_sub_;
-    geometry_msgs::TransformStamped odom_tf_;
-    tf::TransformBroadcaster tf_broadcaster_;
-    ros::Timer heart_timer_;
-    ros::Timer communication_timer_;
-    ros::NodeHandle nh_;
+    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub_;
+    rclcpp::Publisher<geometry_msgs::msg::Pose2D>::SharedPtr uwb_pub_;
+    rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr imu_pub_;
+    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_sub_;
+    rclcpp::Subscription<ackermann_msgs::msg::AckermannDrive>::SharedPtr ackermann_sub_;
+    geometry_msgs::msg::TransformStamped odom_tf_;
+    std::shared_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+    rclcpp::TimerBase::SharedPtr heart_timer_;
+    rclcpp::TimerBase::SharedPtr communication_timer_;
+    rclcpp::Node::SharedPtr nh_;
     Serial serial_;
-    void velocityCallback(const geometry_msgs::Twist::ConstPtr& msg);
-    void ackermannCallback(const ackermann_msgs::AckermannDrive::ConstPtr& msg);
+    void velocityCallback(const geometry_msgs::msg::Twist::SharedPtr& msg);
+    void ackermannCallback(const ackermann_msgs::msg::AckermannDrive::SharedPtr& msg);
     void serialDataProc(uint8_t *data, unsigned int data_len);
     void tianboardDataProc(unsigned char *buf, int len);
-    void heartCallback(const ros::TimerEvent&);
-    void communicationErrorCallback(const ros::TimerEvent&);
+    void heartCallback();
+    void communicationErrorCallback();
 };
 
 #endif
--- tianracer_core/src/tianboard.cpp
+++ tianracer_core/src/tianboard.cpp
@@ -109,8 +109,7 @@ void Tianboard::serialDataProc(uint8_t *data, unsigned int data_len)
             }
             else
             {
-
-                ROS_INFO("BCC error");
+                RCLCPP_INFO(get_logger(), "BCC error");
             }
             state = 0;
         }
@@ -123,232 +122,241 @@ void Tianboard::serialDataProc(uint8_t *data, unsigned int data_len)
     }
 }
 
-void Tianboard::tianboardDataProc(unsigned char *buf, int len)
-{
-    struct protocol_pack *p = (struct protocol_pack *)buf;
-    switch (p->pack_type)
-    {
-    case PACK_TYPE_ODOM_RESPONSE:
-        if (sizeof(struct odom) == p->len - 2)
-        {
-            nav_msgs::Odometry odom_msg;
-            struct odom *pOdom = (struct odom *)(p->data);
-            ros::Time current_time = ros::Time::now();
-            odom_msg.header.stamp = current_time;
-            odom_msg.header.frame_id = "odom";
+// void Tianboard::tianboardDataProc(unsigned char *buf, int len)
+// {
+//     struct protocol_pack *p = (struct protocol_pack *)buf;
+//     switch (p->pack_type)
+//     {
+//     case PACK_TYPE_ODOM_RESPONSE:
+//         if (sizeof(struct odom) == p->len - 2)
+//         {
+//             nav_msgs::msg::Odometry odom_msg;
+//             struct odom *pOdom = (struct odom *)(p->data);
+//             rclcpp::Time current_time = this -> now();
+//             odom_msg.header.stamp = current_time;
+//             odom_msg.header.frame_id = "odom";
 
-            odom_msg.pose.pose.position.x = pOdom->pose.point.x;
-            odom_msg.pose.pose.position.y = pOdom->pose.point.y;
-            odom_msg.pose.pose.position.z = pOdom->pose.point.z;
-            geometry_msgs::Quaternion q = tf::createQuaternionMsgFromYaw(pOdom->pose.yaw);
-            odom_msg.pose.pose.orientation = q;
-            //set the velocity
-            odom_msg.child_frame_id = "base_footprint";
-            odom_msg.twist.twist.linear.x = pOdom->twist.linear.x;
-            odom_msg.twist.twist.linear.y = pOdom->twist.linear.y;
-            odom_msg.twist.twist.linear.z = pOdom->twist.linear.z;
-            odom_msg.twist.twist.angular.x = pOdom->twist.angular.x;
-            odom_msg.twist.twist.angular.y = pOdom->twist.angular.y;
-            odom_msg.twist.twist.angular.z = pOdom->twist.angular.z;
-            //publish the message
-            odom_pub_.publish(odom_msg);
+//             odom_msg.pose.pose.position.x = pOdom->pose.point.x;
+//             odom_msg.pose.pose.position.y = pOdom->pose.point.y;
+//             odom_msg.pose.pose.position.z = pOdom->pose.point.z;
+//             tf2::Quaternion q_odom;
+//             q_odom.setRPY(0.0, 0.0, pOdom->pose.yaw);
+//             geometry_msgs::msg::Quaternion q = tf2::toMsg(q_odom);
+//             odom_msg.pose.pose.orientation = q;
+//             //set the velocity
+//             odom_msg.child_frame_id = "base_footprint";
+//             odom_msg.twist.twist.linear.x = pOdom->twist.linear.x;
+//             odom_msg.twist.twist.linear.y = pOdom->twist.linear.y;
+//             odom_msg.twist.twist.linear.z = pOdom->twist.linear.z;
+//             odom_msg.twist.twist.angular.x = pOdom->twist.angular.x;
+//             odom_msg.twist.twist.angular.y = pOdom->twist.angular.y;
+//             odom_msg.twist.twist.angular.z = pOdom->twist.angular.z;
+//             //publish the message
+//             odom_pub_->publish(odom_msg);
 
-            odom_tf_.header.stamp = current_time;
-            odom_tf_.transform.translation.x = pOdom->pose.point.x;
-            odom_tf_.transform.translation.y = pOdom->pose.point.y;
-            odom_tf_.transform.translation.z = pOdom->pose.point.z;
+//             odom_tf_.header.stamp = current_time;
+//             odom_tf_.transform.translation.x = pOdom->pose.point.x;
+//             odom_tf_.transform.translation.y = pOdom->pose.point.y;
+//             odom_tf_.transform.translation.z = pOdom->pose.point.z;
 
-            odom_tf_.transform.rotation = odom_msg.pose.pose.orientation;
-            //tf_broadcaster_.sendTransform(odom_tf_);
-        }
-        break;
+//             odom_tf_.transform.rotation = odom_msg.pose.pose.orientation;
+//             //tf_broadcaster_.sendTransform(odom_tf_);
+//         }
+//         break;
 
-    case PACK_TYPE_UWB_RESPONSE:
-        if (sizeof(struct uwb) == p->len - 2)
-        {
-            geometry_msgs::Pose2D pose2d_msg;
-            struct uwb *pUwb = (struct uwb *)(p->data);
-            pose2d_msg.x = pUwb->x_m;
-            pose2d_msg.y = pUwb->y_m;
-            pose2d_msg.theta = pUwb->yaw;
-            uwb_pub_.publish(pose2d_msg);
-        }
-        break;
+//     case PACK_TYPE_UWB_RESPONSE:
+//         if (sizeof(struct uwb) == p->len - 2)
+//         {
+//             geometry_msgs::Pose2D pose2d_msg;
+//             struct uwb *pUwb = (struct uwb *)(p->data);
+//             pose2d_msg.x = pUwb->x_m;
+//             pose2d_msg.y = pUwb->y_m;
+//             pose2d_msg.theta = pUwb->yaw;
+//             uwb_pub_.publish(pose2d_msg);
+//         }
+//         break;
 
-    case PACK_TYPE_HEART_BEAT_RESPONSE:
-        break;
+//     case PACK_TYPE_HEART_BEAT_RESPONSE:
+//         break;
 
-    case PACK_TYPE_IMU_REPONSE:
-        if (sizeof(struct imu_feedback) == p->len - 2)
-        {
-            sensor_msgs::Imu imu_msg;
-            struct imu_feedback *pImu = (struct imu_feedback *)(p->data);
+//     case PACK_TYPE_IMU_REPONSE:
+//         if (sizeof(struct imu_feedback) == p->len - 2)
+//         {
+//             sensor_msgs::Imu imu_msg;
+//             struct imu_feedback *pImu = (struct imu_feedback *)(p->data);
 
-            ros::Time current_time = ros::Time::now();
-            imu_msg.header.stamp = current_time;
-            imu_msg.header.frame_id = "imu_link";
-            imu_msg.orientation.x = pImu->quat.x;
-            imu_msg.orientation.y = pImu->quat.y;
-            imu_msg.orientation.z = pImu->quat.z;
-            imu_msg.orientation.w = pImu->quat.w;
-            imu_msg.angular_velocity.x = pImu->angular_vel.x;
-            imu_msg.angular_velocity.y = pImu->angular_vel.y;
-            imu_msg.angular_velocity.z = pImu->angular_vel.z;
-            imu_msg.linear_acceleration.x = pImu->linear_acc.x;
-            imu_msg.linear_acceleration.y = pImu->linear_acc.y;
-            imu_msg.linear_acceleration.z = pImu->linear_acc.z;
-            imu_pub_.publish(imu_msg);
-        }
-        break;
+//             ros::Time current_time = ros::Time::now();
+//             imu_msg.header.stamp = current_time;
+//             imu_msg.header.frame_id = "imu_link";
+//             imu_msg.orientation.x = pImu->quat.x;
+//             imu_msg.orientation.y = pImu->quat.y;
+//             imu_msg.orientation.z = pImu->quat.z;
+//             imu_msg.orientation.w = pImu->quat.w;
+//             imu_msg.angular_velocity.x = pImu->angular_vel.x;
+//             imu_msg.angular_velocity.y = pImu->angular_vel.y;
+//             imu_msg.angular_velocity.z = pImu->angular_vel.z;
+//             imu_msg.linear_acceleration.x = pImu->linear_acc.x;
+//             imu_msg.linear_acceleration.y = pImu->linear_acc.y;
+//             imu_msg.linear_acceleration.z = pImu->linear_acc.z;
+//             imu_pub_->publish(imu_msg);
+//         }
+//         break;
 
-    default:
-        break;
-    }
-    communication_timer_.stop();
-    communication_timer_.start();
-}
+//     default:
+//         break;
+//     }
+//     communication_timer_.stop();
+//     communication_timer_.start();
+// }
 
-void Tianboard::velocityCallback(const geometry_msgs::Twist::ConstPtr &msg)
-{
-    uint16_t len;
-    vector<uint8_t> buf;
+// void Tianboard::velocityCallback(const geometry_msgs::msg::Twist::ConstPtr &msg)
+// {
+//     uint16_t len;
+//     vector<uint8_t> buf;
 
-    uint8_t bcc = 0;
-    struct twist twist;
-    int i;
-    uint8_t *out = (uint8_t *)&twist;
-    twist.linear.x = msg->linear.x;
-    twist.linear.y = msg->linear.y;
-    twist.linear.z = msg->linear.z;
-    twist.angular.x = msg->angular.x;
-    twist.angular.y = msg->angular.y;
-    twist.angular.z = msg->angular.z;
+//     uint8_t bcc = 0;
+//     struct twist twist;
+//     int i;
+//     uint8_t *out = (uint8_t *)&twist;
+//     twist.linear.x = msg->linear.x;
+//     twist.linear.y = msg->linear.y;
+//     twist.linear.z = msg->linear.z;
+//     twist.angular.x = msg->angular.x;
+//     twist.angular.y = msg->angular.y;
+//     twist.angular.z = msg->angular.z;
 
-    buf.push_back(PROTOCOL_HEAD & 0xFF);
-    buf.push_back((PROTOCOL_HEAD >> 8) & 0xFF);
+//     buf.push_back(PROTOCOL_HEAD & 0xFF);
+//     buf.push_back((PROTOCOL_HEAD >> 8) & 0xFF);
 
-    len = sizeof(struct twist) + 2;
+//     len = sizeof(struct twist) + 2;
 
-    buf.push_back(len & 0xFF);
-    buf.push_back((len >> 8) & 0xFF);
+//     buf.push_back(len & 0xFF);
+//     buf.push_back((len >> 8) & 0xFF);
 
-    buf.push_back(PACK_TYPE_CMD_VEL & 0xFF);
-    buf.push_back((PACK_TYPE_CMD_VEL >> 8) & 0xFF);
+//     buf.push_back(PACK_TYPE_CMD_VEL & 0xFF);
+//     buf.push_back((PACK_TYPE_CMD_VEL >> 8) & 0xFF);
 
-    for (i = 0; i < sizeof(struct twist); i++)
-    {
-        buf.push_back(out[i]);
-    }
+//     for (i = 0; i < sizeof(struct twist); i++)
+//     {
+//         buf.push_back(out[i]);
+//     }
 
-    for (i = 4; i < buf.size(); i++)
-    {
-        bcc ^= buf[i];
-    }
+//     for (i = 4; i < buf.size(); i++)
+//     {
+//         bcc ^= buf[i];
+//     }
 
-    buf.push_back(bcc);
+//     buf.push_back(bcc);
 
-    serial_.send(&buf[0], buf.size());
-    heart_timer_.stop();
-    heart_timer_.start();
-}
+//     serial_.send(&buf[0], buf.size());
+//     heart_timer_.stop();
+//     heart_timer_.start();
+// }
 
-void Tianboard::ackermannCallback(const ackermann_msgs::AckermannDrive::ConstPtr &msg)
-{
-    uint16_t len;
-    vector<uint8_t> buf;
+// void Tianboard::ackermannCallback(const ackermann_msgs::AckermannDrive::ConstPtr &msg)
+// {
+//     uint16_t len;
+//     vector<uint8_t> buf;
 
-    uint8_t bcc = 0;
-    struct ackermann_cmd ackermann_cmd;
-    int i;
-    uint8_t *out = (uint8_t *)&ackermann_cmd;
-    ackermann_cmd.steering_angle = msg->steering_angle;
-    ackermann_cmd.speed = msg->speed;
+//     uint8_t bcc = 0;
+//     struct ackermann_cmd ackermann_cmd;
+//     int i;
+//     uint8_t *out = (uint8_t *)&ackermann_cmd;
+//     ackermann_cmd.steering_angle = msg->steering_angle;
+//     ackermann_cmd.speed = msg->speed;
 
-    buf.push_back(PROTOCOL_HEAD & 0xFF);
-    buf.push_back((PROTOCOL_HEAD >> 8) & 0xFF);
+//     buf.push_back(PROTOCOL_HEAD & 0xFF);
+//     buf.push_back((PROTOCOL_HEAD >> 8) & 0xFF);
 
-    len = sizeof(struct ackermann_cmd) + 2;
+//     len = sizeof(struct ackermann_cmd) + 2;
 
-    buf.push_back(len & 0xFF);
-    buf.push_back((len >> 8) & 0xFF);
+//     buf.push_back(len & 0xFF);
+//     buf.push_back((len >> 8) & 0xFF);
 
-    buf.push_back(PACK_TYPE_ACKERMANN_CMD & 0xFF);
-    buf.push_back((PACK_TYPE_ACKERMANN_CMD >> 8) & 0xFF);
+//     buf.push_back(PACK_TYPE_ACKERMANN_CMD & 0xFF);
+//     buf.push_back((PACK_TYPE_ACKERMANN_CMD >> 8) & 0xFF);
 
-    for (i = 0; i < sizeof(struct ackermann_cmd); i++)
-    {
-        buf.push_back(out[i]);
-    }
+//     for (i = 0; i < sizeof(struct ackermann_cmd); i++)
+//     {
+//         buf.push_back(out[i]);
+//     }
 
-    for (i = 4; i < buf.size(); i++)
-    {
-        bcc ^= buf[i];
-    }
+//     for (i = 4; i < buf.size(); i++)
+//     {
+//         bcc ^= buf[i];
+//     }
 
-    buf.push_back(bcc);
+//     buf.push_back(bcc);
 
-    serial_.send(&buf[0], buf.size());
-    heart_timer_.stop();
-    heart_timer_.start();
-}
+//     serial_.send(&buf[0], buf.size());
+//     heart_timer_.stop();
+//     heart_timer_.start();
+// }
 
 
-void Tianboard::heartCallback(const ros::TimerEvent &)
-{
-    uint16_t len;
-    vector<uint8_t> buf;
-    uint16_t dummy = 0;
-    uint8_t bcc = 0;
-    int i;
-    uint8_t *out = (uint8_t *)&dummy;
-    buf.push_back(PROTOCOL_HEAD & 0xFF);
-    buf.push_back((PROTOCOL_HEAD >> 8) & 0xFF);
+// void Tianboard::heartCallback()
+// {
+//     uint16_t len;
+//     vector<uint8_t> buf;
+//     uint16_t dummy = 0;
+//     uint8_t bcc = 0;
+//     int i;
+//     uint8_t *out = (uint8_t *)&dummy;
+//     buf.push_back(PROTOCOL_HEAD & 0xFF);
+//     buf.push_back((PROTOCOL_HEAD >> 8) & 0xFF);
 
-    len = 2 + sizeof(dummy);
+//     len = 2 + sizeof(dummy);
 
-    buf.push_back(len & 0xFF);
-    buf.push_back((len >> 8) & 0xFF);
+//     buf.push_back(len & 0xFF);
+//     buf.push_back((len >> 8) & 0xFF);
 
-    buf.push_back(PACK_TYPE_HEART_BEAT & 0xFF);
-    buf.push_back((PACK_TYPE_HEART_BEAT >> 8) & 0xFF);
+//     buf.push_back(PACK_TYPE_HEART_BEAT & 0xFF);
+//     buf.push_back((PACK_TYPE_HEART_BEAT >> 8) & 0xFF);
 
-    for (i = 0; i < sizeof(dummy); i++)
-    {
-        buf.push_back(out[i]);
-    }
+//     for (i = 0; i < sizeof(dummy); i++)
+//     {
+//         buf.push_back(out[i]);
+//     }
 
-    for (i = 4; i < buf.size(); i++)
-    {
-        bcc ^= buf[i];
-    }
+//     for (i = 4; i < buf.size(); i++)
+//     {
+//         bcc ^= buf[i];
+//     }
 
-    buf.push_back(bcc);
+//     buf.push_back(bcc);
 
-    serial_.send(&buf[0], buf.size());
-    heart_timer_.stop();
-    heart_timer_.start();
-}
+//     serial_.send(&buf[0], buf.size());
+//     heart_timer_.stop();
+//     heart_timer_.start();
+// }
 
-void Tianboard::communicationErrorCallback(const ros::TimerEvent &)
-{
-    ROS_ERROR_THROTTLE(5, "Communication with base error");
-}
+// void Tianboard::communicationErrorCallback()
+// {
+//     ROS_ERROR_THROTTLE(5, "Communication with base error");
+// }
 
-Tianboard::Tianboard(ros::NodeHandle *nh) : nh_(*nh)
+Tianboard::Tianboard(): Node("tianracer")
 {
     std::string param_serial_port;
+    auto qos = rclcpp::QoS(rclcpp::KeepLast(10));
 
-    nh_.param<std::string>("serial_port", param_serial_port, DEFAULT_SERIAL_DEVICE);
+    // nh_.param<std::string>("serial_port", param_serial_port, DEFAULT_SERIAL_DEVICE);
 
-    odom_pub_ = nh_.advertise<nav_msgs::Odometry>("odom", 5);
-    imu_pub_ = nh_.advertise<sensor_msgs::Imu>("imu", 5);
-    uwb_pub_ = nh_.advertise<geometry_msgs::Pose2D>("uwb", 5);
+    odom_pub_ = this->create_publisher<nav_msgs::msg::Odometry>("odom", qos);
+    imu_pub_ = this->create_publisher<sensor_msgs::msg::Imu>("imu", qos);
+    uwb_pub_ = this->create_publisher<geometry_msgs::msg::Pose2D>("uwb", qos);
     //cmd_vel_sub_ = nh_.subscribe("cmd_vel", 1, &Tianboard::velocityCallback, this);
-    ackermann_sub_ = nh_.subscribe("ackermann_cmd", 5, &Tianboard::ackermannCallback, this);
-    heart_timer_ = nh_.createTimer(ros::Duration(0.2), &Tianboard::heartCallback, this);
-    heart_timer_.start();
-    communication_timer_ = nh_.createTimer(ros::Duration(0.2), &Tianboard::communicationErrorCallback, this);
-    communication_timer_.start();
+    ackermann_sub_ = this->create_subscription<ackermann_msgs::msg::AckermannDrive>(
+        "ackermann_cmd", \
+         rclcpp::SensorDataQoS(), \
+         std::bind(
+             &Tianboard::ackermannCallback, \
+             this, \
+             std::placeholders::_1));
+    heart_timer_ = this->create_wall_timer(200ms, std::bind(&Tianboard::heartCallback, this));
+    // heart_timer_.start();
+    communication_timer_ = this->create_wall_timer(200ms, std::bind(&Tianboard::communicationErrorCallback, this);
+    // communication_timer_.start();
     odom_tf_.header.frame_id = "odom";
     odom_tf_.child_frame_id = "base_footprint";
 
