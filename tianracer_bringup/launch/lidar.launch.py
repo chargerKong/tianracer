995efda415d461fbe339cbc32e0183f9d81983b9
Liangqian <chargerKong@126.com>
2021-10-30 16:14:04 +0800

    add tianracer_bringup

--- tianracer_bringup/launch/lidar.launch.py
+++ tianracer_bringup/launch/lidar.launch.py
@@ -5,22 +5,19 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
-from launch.substitutions import LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
 def generate_launch_description():
     lidar = os.environ.get("TIANRACER_LIDAR", "rplidar_a1")
     model = os.environ.get("TIANRACER_LIDAR_MODEL", "a1")
-
+    ld = LaunchDescription()
     if "rplidar" in lidar:
-        include_file = IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([get_package_share_directory("tianracer_bringup"),
-             'launch','lidar', 'launch_test.launch.py']),
-             launch_arguments={'model': LaunchConfiguration(model)}.items(),
-        ),
+        ld.add_action(IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(os.path.join(
+                get_package_share_directory("tianracer_bringup"),
+                'launch','includes','lidar', 'rplidar.launch.py')),
+                launch_arguments={'model': model}.items(),
+            ))
 
-    return LaunchDescription([
-        include_file
-        ])
-
+    return ld
